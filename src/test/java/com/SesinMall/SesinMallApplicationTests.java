218389cae9c9a43401c29e9b1ea2de38bc41ce49
junyoung <junyoungs7@naver.com>
2022-08-16 14:14:54 +0900

    Initial commit

---
+++ src/test/java/com/SesinMall/SesinMallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SesinMall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SesinMallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
